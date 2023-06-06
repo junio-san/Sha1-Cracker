68cfc34b2c8505363546f1a10ec2a5c90f325060
oblivion <oblivionccyber@gmail.com>
2023-06-06 01:29:00 -0300

    feat: create Error Handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::env;
+use std::{
+    error::Error,
+};
 
-fn main() {
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 { println!
         ("Use:"); println!
         ("sha1-cracker: <wordlist.txt> <sha1_hash>");       
-        return;
+        return Ok(());
     }
+
+    let hash_to_crack: &str = args[2].trim(); if
+    hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is invalid".into());
+    }
+    Ok(())
 }
+
