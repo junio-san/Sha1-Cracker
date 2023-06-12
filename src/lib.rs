7127e1a88cc1df28c78e03d92ca743cb92dd3f0f
Junio Santos <git@junio.dev>
2023-06-11 22:02:13 -0300

    feat: clap args correctly implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,66 @@
-pub mod config;
+use {
+    std::path::PathBuf,
+    clap::{arg, Command, value_parser},
+};
+mod config;
+use config::Config;
 
-#[allow(unused_imports)]
-use std::io::BufRead;
-use sha2::{Sha256, Digest};
+static STRING_LENGTH: usize = 64;
 
-// Search through file lines checking if matches hash input
-pub fn search(content: String, hash_value: &str) {
-    let val = content
-        .lines()
-        .find(|line| {
+pub enum CLIOptions {
+    Find(Config),
+    Parse(String),
+    Type(String),
+    Example
+}
 
-            let hash = hex::encode(Sha256::digest(line));
-            hash_value == hash
-        });
+/// parse arguments into it
+pub fn cli() -> Command {
+    Command::new("srs")
+        .about("A fictional versioning CLI")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("find")
+                .about("Find word that matches hash function in a file")
+                .arg(arg!(path: [PATH])
+                    .num_args(1)
+                    .value_parser(value_parser!(PathBuf))
+                    .group("FIND")
+                )
+                .arg_required_else_help(true)
+                .arg(arg!(hash: [HASH])
+                    .require_equals(false)
+                    .num_args(1)
+                    .value_parser(value_parser!(String))
+                    .group("FIND")
+                )
+                .arg_required_else_help(true)
+                .arg(custom_type())
+        )
+        .subcommand(
+            Command::new("parse")
+                .about("Parse word using SHA256 hash function")
+                .arg(arg!(value: <VALUE> "Hash value"))
+                .arg(custom_type())
+                .arg_required_else_help(true),
+        )
+        .subcommand(
+            Command::new("example")
+                .about("Run example with ")
+                .arg(arg!(value: <VALUE> "Hash value")
+                    .exclusive(true)
+                )
+        )
+}
 
-    match val {
-        Some(pass) => print!("Password Found: {pass}"),
-        None => println!("Password not found"),
-    };
+fn custom_type() -> clap::Arg {
+    arg!(type: [TYPE])
+        .value_parser(["md5", "sha1", "sha256", "sha512"])
+        .num_args(1)
+        .require_equals(false)
+        .required(false)
+        .default_value("Sha256")
+        .value_parser(value_parser!(String))
 }
